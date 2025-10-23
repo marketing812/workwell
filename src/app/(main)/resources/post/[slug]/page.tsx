31c3a6d04fb00910da8a7c94b68c1cba366859a7
Futuver <marketing@futuver.com>
2025-10-23 09:56:16 +0000

    insisto, no estás usando la url que te viene en la api

--- src/app/(main)/resources/post/[slug]/page.tsx
+++ src/app/(main)/resources/post/[slug]/page.tsx
@@ -46,7 +46,7 @@ export default async function Page({ params }: PageProps) {
       notFound();
   }
 
-  const imageUrl = post._embedded?.['wp:featuredmedia']?.[0]?.source_url || 'https://workwellfut.com/imgapp/800x300/default_800x300.jpg';
+  const imageUrl = post._embedded?.['wp:featuredmedia']?.[0]?.source_url;
 
 
   return (
