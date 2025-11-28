6f036925c529b115e8038ae9c0abd6316e5678d9
Your Name <you@example.com>
2025-11-28 10:43:46 +0000

    correciones ruta2

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -59,3 +59,4 @@ export default function MainAppLayout({ children }: { children: ReactNode }) {
     </TooltipProvider>
   );
 }
+export const dynamic = 'force-dynamic';
--- workspace/src/app/(main)/assessment/intro/page.tsx
+++ workspace/src/app/(main)/assessment/intro/page.tsx
@@ -90,6 +90,7 @@ export default function AssessmentIntroPage() {
             </Link>
           </Button>
         </CardFooter>
+        
       </Card>
     </div>
   );
