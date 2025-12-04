2c72ed4bb165976f2a6632a6c73e513f36c7d692
Futuver <marketing@futuver.com>
2025-12-04 15:07:49 +0000

    añade el audio https://workwellfut.com/audios/ruta3/tecnicas/Ruta3sesion

--- src/components/paths/TwoMinuteRuleExercise.tsx
+++ src/components/paths/TwoMinuteRuleExercise.tsx
@@ -58,6 +58,14 @@ ${when}
           <Edit3 className="mr-2" />
           {content.title}
         </CardTitle>
+        {content.audioUrl && (
+          <div className="mt-4">
+              <audio controls controlsList="nodownload" className="w-full">
+                  <source src={content.audioUrl} type="audio/mp3" />
+                  Tu navegador no soporta el elemento de audio.
+              </audio>
+          </div>
+        )}
         {content.objective && <CardDescription className="pt-2">{content.objective}</CardDescription>}
       </CardHeader>
       <CardContent>
