a768eca0863278141385c0bea522a71d584b4fac
Futuver <marketing@futuver.com>
2025-12-17 10:53:45 +0000

    elimina el segundo audio (_for element <CardDescription>_)

--- src/components/paths/PresentVsEssentialSelfExercise.tsx
+++ src/components/paths/PresentVsEssentialSelfExercise.tsx
@@ -51,7 +51,7 @@ ${smallAction}
   };
 
   const renderStep = () => {
-    switch(step) {
+    switch (step) {
       case 0: // Intro
         return (
           <div className="p-4 space-y-4 text-center">
@@ -106,7 +106,7 @@ ${smallAction}
     <Card className="bg-muted/30 my-6 shadow-md">
       <CardHeader>
         <CardTitle className="text-lg text-accent flex items-center"><Edit3 className="mr-2"/>{content.title}</CardTitle>
-        {content.objective && <CardDescription className="pt-2">{content.objective}<div className="mt-4"><audio controls controlsList="nodownload" className="w-full"><source src="https://workwellfut.com/audios/ruta7/tecnicas/Ruta7semana2tecnica2.mp3" type="audio/mp3" />Tu navegador no soporta el elemento de audio.</audio></div></CardDescription>}
+        {content.objective && <CardDescription className="pt-2">{content.objective}</CardDescription>}
         {content.audioUrl && (
           <div className="mt-4">
               <audio controls controlsList="nodownload" className="w-full">
