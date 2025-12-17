e4ac491c9d9f03b7eb40411be71dc2771b008722
Futuver <marketing@futuver.com>
2025-12-17 10:51:19 +0000

    añade a continuación del texto, el audio: https://workwellfut.com/audios

--- src/components/paths/PresentVsEssentialSelfExercise.tsx
+++ src/components/paths/PresentVsEssentialSelfExercise.tsx
@@ -106,7 +106,7 @@ ${smallAction}
     <Card className="bg-muted/30 my-6 shadow-md">
       <CardHeader>
         <CardTitle className="text-lg text-accent flex items-center"><Edit3 className="mr-2"/>{content.title}</CardTitle>
-        {content.objective && <CardDescription className="pt-2">{content.objective}</CardDescription>}
+        {content.objective && <CardDescription className="pt-2">{content.objective}<div className="mt-4"><audio controls controlsList="nodownload" className="w-full"><source src="https://workwellfut.com/audios/ruta7/tecnicas/Ruta7semana2tecnica2.mp3" type="audio/mp3" />Tu navegador no soporta el elemento de audio.</audio></div></CardDescription>}
         {content.audioUrl && (
           <div className="mt-4">
               <audio controls controlsList="nodownload" className="w-full">
