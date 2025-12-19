f78a7a2feb09d6388ec63f5ae803b839448f39bc
Futuver <marketing@futuver.com>
2025-12-19 13:17:28 +0000

    añade antes del texto el audio: https://workwellfut.com/audios/ruta8/des

--- src_root/types/page-props.ts
+++
@@ -1,6 +0,0 @@
-// Definiendo un tipo genérico para las propiedades de las páginas
-// Esto ayuda a manejar tanto las propiedades estáticas como las dinámicas de Next.js
-export type RoutePageProps<T = Record<string, string>> = {
-  params: T;
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
