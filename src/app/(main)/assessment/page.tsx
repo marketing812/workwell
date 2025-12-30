c3c12ab278a08e165f2ede14c3115bb21989f62a
Futuver <marketing@futuver.com>
2025-12-30 16:29:09 +0000

    has vuelto a quitar en la evaluacion que las preguntas se guarden tempor

--- src/app/(main)/assessment/page.tsx
+++ src/app/(main)/assessment/page.tsx
@@ -1,14 +1,11 @@
 
 "use client";
 import AssessmentPageClient from '@/components/assessment/AssessmentPageClient';
-import { assessmentDimensions } from '@/data/assessmentDimensions';
 
 export default function AssessmentPage() {
   return (
     <AssessmentPageClient
-      assessmentDimensions={assessmentDimensions}
       isGuided={false}
     />
   );
 }
-
