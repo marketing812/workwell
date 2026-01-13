9cd9c8db6fbc8c7e2d1bc5048b7a488aa34c149d
Futuver <marketing@futuver.com>
2026-01-13 10:58:40 +0000

    [GoogleGenerativeAI Error]: Error fetching from https://generativelangua

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,8 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({ apiKey: '' }), // Forzar el uso de ADC en el entorno de App Hosting
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
